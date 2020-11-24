2f6628d8c6678fed1fb383577e12b71d102e4eec
Matsuda1129 <mk1529ayukat315takoyaki@yahoo.co.jp>
2020-11-25 07:18:32 +0900

    完成③

--- throwing-money-app/src/templates/UsersList.jsx
+++ throwing-money-app/src/templates/UsersList.jsx
@@ -1,33 +1,40 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsersList } from "../reducks/userslist/operations";
 import { signOut } from "../reducks/users/operations";
-import { getUserslist } from "../reducks/userslist/selectors"
-import { getUserId, getUsername, getMoney } from '../reducks/users/selectors';
-import Modal from 'react-modal'
-import { Wallet, Remittance } from "../components/Modal"
+import { getUserslist } from "../reducks/userslist/selectors";
+import { getUserId, getUsername, getMoney } from "../reducks/users/selectors";
+import Modal from "react-modal";
+import { Wallet, Remittance } from "../components/Modal";
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
 const UsersList = (props) => {
   const dispatch = useDispatch();
   const selector = useSelector((state) => state);
-  const uid = getUserId(selector)
-  const username = getUsername(selector)
-  const money = getMoney(selector)
+  const uid = getUserId(selector);
+  const username = getUsername(selector);
   const usersList = getUserslist(selector);
 
   const users = usersList.filter((user, index) => {
     return user.uid !== uid;
   });
 
-  useEffect(() => {
-    dispatch(fetchUsersList())
+  const you = usersList.filter((user, index) => {
+    return user.uid === uid;
   });
+
+  useEffect(() => {
+    dispatch(fetchUsersList());
+  });
+
+  const money = you.map((you) => you.money);
 
   return (
     <section>
-      <p>ようこそ、{username}さん　　残高:{money}</p>
+      <p>
+        ようこそ、{username}さん　　残高:{money}
+      </p>
       <button onClick={() => dispatch(signOut())}>SIGN OUT</button>
       <table>
         <thead>
@@ -36,27 +43,30 @@ const UsersList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {users.length > 0 && (
-            users.map(user => (
+          {users.length > 0 &&
+            users.map((user) => (
               <tr key={user.uid}>
                 <td>{user.username}</td>
                 <td>
                   <Wallet name={user.username} money={user.money} />
                 </td>
                 <td>
-                  <Remittance yourMoney={money} othersMoney={user.money} yourUid={uid} othersUid={user.uid} />
+                  <Remittance
+                    yourMoney={money}
+                    othersMoney={user.money}
+                    yourUid={uid}
+                    othersUid={user.uid}
+                  />
                 </td>
               </tr>
-            ))
-          )}
+            ))}
         </tbody>
         <tfoot>
-          <tr>
-          </tr>
+          <tr></tr>
         </tfoot>
       </table>
-    </section >
+    </section>
   );
 };
 
-export default UsersList
+export default UsersList;
